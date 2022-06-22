3486fdd754e430155b47aaec58b697405b32cd3f
seiyarick <seiyahirota2@gmail.com>
2022-06-22 12:40:48 +0900

    code1-4 java.util.Arrays equals

--- src/equals2/Main.java
+++ src/equals2/Main.java
@@ -9,11 +9,11 @@ public class Main {
 		h1.name = "ミナト";
 		list.add(h1); // インスタンスを作って格納,Listクラスのaddメソッドを使用（単数の値をリストに追加するメソッド）
 		System.out.println(h1.name);
-		System.out.println("要素数=" + list.size());
+		System.out.println("要素数=" + list.size()); // 「sizeメソッド=>要素数を確認」
 		h1 = new Hero();
 		h1.name = "ミナト";
-		list.remove(h1);
-		System.out.println("要素数=" + list.size());
+		list.remove(h1); // 「remove()メソッド=>これと同じものを削除して」名前が"ミナト"の勇者を削除
+		System.out.println("要素数=" + list.size());// [要素数=0]になってほしいが結果は[要素数=1]
 	}
 
 }
