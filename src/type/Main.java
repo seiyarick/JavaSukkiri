f7eacf61c214d87e75de9f8f427f0353a1cb548e
seiyarick <seiyahirota2@gmail.com>
2022-06-30 17:39:32 +0900

    code2-2 Pocket

--- src/type/Main.java
+++ src/type/Main.java
@@ -6,14 +6,16 @@ public class Main {
 	// 第一引数の文字列を第二引数の回数だけ表示する
 	// 第一引数には文字列の情報を。第二引数には整数を指定してください
 	public static void prints(Object a, Object b) {
-		for (int i = 0; i < (Integer)b; i++) {
+		for (int i = 0; i < (int)b; i++) {
 			System.out.println(a);
 		}
 	}
 	public static void main(String[] args) {
+		// sには文字列も格納可能
 		Object s = "こんにちは";
 		s = new Hero();
 		Object n = 1;
+		// 注意深く呼び出す必要がある
 		prints(s,n);
 	}
 	
