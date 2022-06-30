7fbadc3a8ed7e543e2a54340763f3de138b4c02f
seiyarick <seiyahirota2@gmail.com>
2022-06-30 17:50:53 +0900

    code2-3 Pocket

--- src/generics/Pocket.java
+++ src/generics/Pocket.java
@@ -1,5 +1,16 @@
 package generics;
-
+// ジェネリクスを使わないPocketクラス（java1.4まで）
+// どんな型のインスタンスでも格納できる
+// 格納するためのputメソッド、取り出すためのgetメソッドがある
 public class Pocket {
+	private Object data;
+	// 格納するためのput()メソッド
+	public void put(Object d) {
+		this.data = d;
+		}
+	// 取り出すためのget()メソッド
+	public Object get() {
+		return this.data;
+		}
 
 }
