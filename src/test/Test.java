29f4fcde3425fdbc5f6688d39232ade746b14168
seiyarick <seiyahirota2@gmail.com>
2022-07-16 16:10:48 +0900

    test

---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test {
+	
+		public static void main(String[] args) {
+		  for(int i = 1; i <= 3; i++) {
+			  for(int j = 3; j >= 1; j--) {
+				  if(i !=j ) {
+					  System.out.println("(" + i + "," + j + ")");
+				  }
+			  }
+			  System.out.println();
+		  }
+		}
+
+}
