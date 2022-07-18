79ecad4dbc71684b81253fc2e9328ab5024f2e35
seiyarick <seiyahirota2@gmail.com>
2022-07-18 12:23:52 +0900

    復習

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,7 @@ public class Test {
 		public static void main(String[] args) {
 		  for(int i = 1; i <= 3; i++) {
 			  for(int j = 3; j >= 1; j--) {
-				  if(i !=j ) {
+				  if(i != j ) {
 					  System.out.println("(" + i + "," + j + ")");
 				  }
 			  }
