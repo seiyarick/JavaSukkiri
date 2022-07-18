ba01f025f2b486d57eafffb08e1f2fafdd780079
seiyarick <seiyahirota2@gmail.com>
2022-07-18 12:41:18 +0900

    ４章　確認問題

---
+++ src/Chapter04.java
@@ -0,0 +1,19 @@
+//問題
+//・自分の名前を「String」型の変数に代入して出力
+//・好きな言葉を同じ変数に再代入して出力
+//・ラッキーナンバーとして「int」型で宣言した定数を出力
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String s = "太郎";
+		System.out.println(s);
+		
+		s = "二郎";
+		System.out.println(s);
+		
+		final int LACKEY;
+		LACKEY = 10;
+		System.out.println(LACKEY);
+	}
+
+}
