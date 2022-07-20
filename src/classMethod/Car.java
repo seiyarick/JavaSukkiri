639f0e27d9e88d18d3ff78c19b4d93137cd25e23
seiyarick <seiyahirota2@gmail.com>
2022-07-20 18:56:23 +0900

    9章　確認問題　クラスとメソッドを理解しよう

---
+++ src/classMethod/Car.java
@@ -0,0 +1,22 @@
+package classMethod;
+
+public class Car {
+	private double fuelCost;
+	private double fuelAmount;
+	
+	public Car(double c, double a) {
+		this.fuelCost = c;
+		this.fuelAmount = a;
+	}
+	
+	public void move(int km) {
+		System.out.println(km + "km走ります");
+		this.fuelAmount = fuelAmount - (km / fuelCost); 
+	}
+	
+	public double getFuelAmount() {
+		return this.fuelAmount;
+		
+	}
+
+}
